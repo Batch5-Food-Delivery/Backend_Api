0680bdc1b726f459600c68c0ab1ed8396715bb6a
SwamYi <swamyi@gmail.com>
2024-08-13 10:59:24 +0630

    user table renamed and data.sql created

--- src/main/java/com/hostmdy/food/domain/User.java
+++ src/main/java/com/hostmdy/food/domain/User.java
@@ -1,9 +1,11 @@
 package com.hostmdy.food.domain;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 	
 	@Id
