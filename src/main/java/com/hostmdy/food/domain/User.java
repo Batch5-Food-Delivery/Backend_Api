1951314439e9f9f957b8da52f44de4764f7d681f
SwamYi <swamyi@gmail.com>
2024-08-23 00:42:38 +0630

    username field added to User

--- src/main/java/com/hostmdy/food/domain/User.java
+++ src/main/java/com/hostmdy/food/domain/User.java
@@ -27,6 +27,7 @@ public class User {
 	
 	private String firstname;
 	private String lastname;
+	private String username;
 	private String email;
 	private String password;
 	private Boolean enable;
